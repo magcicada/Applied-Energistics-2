dffd0287fc677609555db4de7c1b6b9d64ec77d9
zeng-git <95841646+zeng-github01@users.noreply.github.com>
2024-11-03 14:05:14 +0800

    Add upgrade slot for Wireless Interface Terminal

--- src/main/java/appeng/container/implementations/ContainerWirelessInterfaceTerminal.java
+++ src/main/java/appeng/container/implementations/ContainerWirelessInterfaceTerminal.java
@@ -2,21 +2,34 @@ package appeng.container.implementations;
 
 import appeng.api.config.Actionable;
 import appeng.api.config.PowerMultiplier;
+import appeng.api.implementations.IUpgradeableCellContainer;
 import appeng.api.networking.security.IActionHost;
 import appeng.container.interfaces.IInventorySlotAware;
+import appeng.container.slot.SlotRestrictedInput;
 import appeng.core.AEConfig;
 import appeng.core.localization.PlayerMessages;
 import appeng.helpers.WirelessTerminalGuiObject;
+import appeng.parts.automation.StackUpgradeInventory;
+import appeng.tile.inventory.AppEngInternalInventory;
 import appeng.util.Platform;
+import appeng.util.inv.IAEAppEngInventory;
+import appeng.util.inv.InvOperation;
 import baubles.api.BaublesApi;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.ClickType;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.items.IItemHandler;
+import org.jetbrains.annotations.NotNull;
 
-public class ContainerWirelessInterfaceTerminal extends ContainerInterfaceTerminal implements IInventorySlotAware {
+public class ContainerWirelessInterfaceTerminal extends ContainerInterfaceTerminal implements IInventorySlotAware, IUpgradeableCellContainer, IAEAppEngInventory {
     private final WirelessTerminalGuiObject wirelessTerminalGUIObject;
     private final int slot;
     private double powerMultiplier = 0.5;
     private int ticks = 0;
+    protected AppEngInternalInventory upgrades;
+    protected SlotRestrictedInput magnetSlot;
 
     public ContainerWirelessInterfaceTerminal(InventoryPlayer ip, WirelessTerminalGuiObject guiObject) {
         super(ip, guiObject,false);
@@ -35,6 +48,11 @@ public class ContainerWirelessInterfaceTerminal extends ContainerInterfaceTermin
         this.bindPlayerInventory(ip,0,0);
 
         this.wirelessTerminalGUIObject = guiObject;
+
+        upgrades = new StackUpgradeInventory(wirelessTerminalGUIObject.getItemStack(), this, 2);
+        this.loadFromNBT();
+        setupUpgrades();
+
     }
 
     @Override
@@ -89,6 +107,33 @@ public class ContainerWirelessInterfaceTerminal extends ContainerInterfaceTermin
         }
     }
 
+    @Override
+    public ItemStack slotClick(int slotId, int dragType, ClickType clickTypeIn, @NotNull EntityPlayer player) {
+        if (slotId >= 0 && slotId < this.inventorySlots.size()) {
+            if (clickTypeIn == ClickType.PICKUP && dragType == 1) {
+                if (this.inventorySlots.get(slotId) == magnetSlot) {
+                    ItemStack itemStack = magnetSlot.getStack();
+                    if (!magnetSlot.getStack().isEmpty()) {
+                        NBTTagCompound tag = itemStack.getTagCompound();
+                        if (tag == null) {
+                            tag = new NBTTagCompound();
+                        }
+                        if (tag.hasKey("enabled")) {
+                            boolean e = tag.getBoolean("enabled");
+                            tag.setBoolean("enabled", !e);
+                        } else {
+                            tag.setBoolean("enabled", false);
+                        }
+                        magnetSlot.getStack().setTagCompound(tag);
+                        magnetSlot.onSlotChanged();
+                        return ItemStack.EMPTY;
+                    }
+                }
+            }
+        }
+        return super.slotClick(slotId, dragType, clickTypeIn, player);
+    }
+
     private double getPowerMultiplier() {
         return this.powerMultiplier;
     }
@@ -112,4 +157,42 @@ public class ContainerWirelessInterfaceTerminal extends ContainerInterfaceTermin
         return wirelessTerminalGUIObject.isBaubleSlot();
     }
 
+    @Override
+    public int availableUpgrades() {
+        return 1;
+    }
+
+    @Override
+    public void setupUpgrades() {
+        if (wirelessTerminalGUIObject != null) {
+            for (int upgradeSlot = 0; upgradeSlot < availableUpgrades(); upgradeSlot++) {
+                this.magnetSlot = new SlotRestrictedInput(SlotRestrictedInput.PlacableItemType.UPGRADES, upgrades, upgradeSlot, 183, -1 + upgradeSlot * 18, this.getInventoryPlayer());
+                this.magnetSlot.setNotDraggable();
+                this.addSlotToContainer(magnetSlot);
+            }
+        }
+    }
+
+    private void loadFromNBT() {
+        NBTTagCompound data = wirelessTerminalGUIObject.getItemStack().getTagCompound();
+        if (data != null) {
+            upgrades.readFromNBT(wirelessTerminalGUIObject.getItemStack().getTagCompound().getCompoundTag("upgrades"));
+        }
+    }
+
+    @Override
+    public void saveChanges() {
+        if (Platform.isServer()) {
+            NBTTagCompound tag = new NBTTagCompound();
+
+            this.upgrades.writeToNBT(tag, "upgrades");
+
+            this.wirelessTerminalGUIObject.saveChanges(tag);
+        }
+    }
+
+    @Override
+    public void onChangeInventory(IItemHandler inv, int slot, InvOperation mc, ItemStack removedStack, ItemStack newStack) {
+
+    }
 }
