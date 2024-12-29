9ee56c668bd2082fa0bfe28b7be919464eedbeb9
zeng-git <95841646+zeng-github01@users.noreply.github.com>
2024-12-30 00:23:12 +0800

    Fix #520

--- src/main/java/appeng/helpers/DualityInterface.java
+++ src/main/java/appeng/helpers/DualityInterface.java
@@ -1158,7 +1158,7 @@ public class DualityInterface implements IGridTickable, IStorageMonitorable, IIn
             }
             case LOCK_UNTIL_RESULT -> {
                 unlockEvent = UnlockCraftingEvent.RESULT;
-                unlockStack = pattern.getPrimaryOutput();
+                unlockStack = pattern.getPrimaryOutput().copy();
                 saveChanges();
             }
         }
